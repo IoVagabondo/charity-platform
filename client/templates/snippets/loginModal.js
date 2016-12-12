f8dd94ce1f2b0a3251ac84b583519771b597029d
Sven Rossmann <scrossmann@gmail.com>
2016-12-13 00:44:11 +0100

    Implemented login modal

---
+++ client/templates/snippets/loginModal.js
@@ -0,0 +1,18 @@
+Template.loginModal.events({
+    'submit form': function(event, template) {
+        event.preventDefault();
+        var emailVar = event.target.username.value;
+        var passwordVar = event.target.password.value;
+
+        Meteor.loginWithPassword(emailVar, passwordVar, function(error){
+        	if (error) {
+        		Bert.alert(error.reason, "warning");
+        	}
+        	else{
+            $('#loginModal').modal('hide');
+        		Router.go('Home');
+        	}
+        });
+
+    }
+});
