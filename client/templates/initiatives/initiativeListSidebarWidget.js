727f105f7caabd32e4e5209e42a974579010010d
Sven Rossmann <scrossmann@gmail.com>
2016-12-12 01:11:15 +0100

    Dynamic loading of sections and categories into sidebar widget

--- client/templates/initiatives/initiativeListSidebarWidget.js
+++ client/templates/initiatives/initiativeListSidebarWidget.js
@@ -1,3 +1,27 @@
+Template.initiativeListSidebarWidgets.onCreated(function(){
+  let template = Template.instance();
+
+      template.subscribe('categories');
+      template.subscribe('sections');
+      template.subscribe('cities');
+
+});
+
+
+Template.initiativeListSidebarWidgets.helpers({
+
+    categoriesList: function() {
+        return Categories.find();
+    },
+
+    sectionsList: function() {
+        return Sections.find();
+    },
+
+    citiesList: function() {
+        return Cities.find();
+    },
+});
 
 Template.initiativeListSidebarWidgets.events({
   'click #suggestNGO': function(event, template) {
